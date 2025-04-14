e8f4195b61cf2d0e97d3cba33374d73103bc4436
Marcos Schwaab <marcosschwaab@MacBook-Air-de-Marcos.local>
2025-04-14 20:25:05 -0300

    chore: configure Eslint and config database

---
+++ src/config/database.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); 
+
+const dataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT || '3306'),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  synchronize: false, 
+  logging: true,
+
+});
+
+export default dataSource;
