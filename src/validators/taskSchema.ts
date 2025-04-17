6ac0759d2e29072d7454f7ef79142081f07b919e
Marcos Schwaab <marcosschwaab@MacBook-Air-de-Marcos.local>
2025-04-16 22:14:15 -0300

    feat(task): add more validates for task

--- src/validators/taskSchema.ts
+++ src/validators/taskSchema.ts
@@ -3,4 +3,6 @@ import { z } from 'zod';
 export const createTaskSchema = z.object({
   title: z.string().min(1, 'tittle is required'),
   description: z.string().min(1, 'description is required'),
+  status: z.enum(['todo', 'in_progress', 'done']).optional(),
+  priority: z.enum(['low', 'medium', 'high', 'critical']).optional(),
 });
