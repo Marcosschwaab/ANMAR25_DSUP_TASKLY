27c261fc14e11e7910b05e1824434aab0d4a9fda
Marcos Schwaab <marcosschwaab@MacBook-Air-de-Marcos.local>
2025-04-17 01:15:11 -0300

    feat: create validate trim() for whitespaces

--- src/validators/noteSchema.ts
+++ src/validators/noteSchema.ts
@@ -2,5 +2,5 @@ import { z } from 'zod';
 import { Note } from '../entities/Note';
 
 export const valuesNoteSchema = z.object({
-  content: z.string().min(5).max(255),
+  content: z.string().trim().min(5).max(255),
 });
--- src/validators/taskSchema.ts
+++ src/validators/taskSchema.ts
@@ -5,8 +5,8 @@ const statusValues = ['todo', 'in_progress', 'done'] as const;
 const priorityValues = ['low', 'medium', 'high', 'critical'] as const;
 
 export const valuesTaskSchema = z.object({
-  title: z.string().min(3).max(255),
-  description: z.string().min(5),
+  title: z.string().trim().min(3).max(255),
+  description: z.string().trim().min(5).max(255),
   status: z.enum(statusValues).optional().default('todo'),
   priority: z.enum(priorityValues).optional().default('medium'),
 });
